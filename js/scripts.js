35f58a104cd28718da53eeba0f753ccde1c4bcfa
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2017-02-06 18:15:59 -0200

    Update layout colors and style

--- js/scripts.js
+++ js/scripts.js
@@ -48,16 +48,3 @@ $(document).ready(function(){
 
 });
 
-$(window).load(function(){  
-  for (var i = 0; i < 50; i++) {
-    var names = ['x1','x2','y1','y2'],
-        name = names[Math.floor(Math.random() * names.length)];
-    $('.circles').append('<div class="circle-container c'+i+'"><div class="circle i'+ i +'"></div></div>');
-    $('.c'+i).css({
-      'animation': 'z 10s .'+ i +'s linear infinite'
-    });
-    $('.i'+i).css({
-      'animation': name + ' 10.5s .'+ i +'s linear infinite'
-    });
-  }
-});
