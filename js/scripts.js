6130703a6e6e7177f232c821d213a40afcafc85a
Rodrigo Teixeira <drigoteixeira@gmail.com>
2020-05-20 12:55:29 -0300

    Update style and homepage

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
     
     WebFont.load({
     google: {
-      families: ['Noto+Serif:400,400i,700,700i', 'Heebo:400,700'],
+      families: ['Noto+Serif:400,400i,700,700i', 'Poppins:400,400i,700,700i'],
       url: ['https://fonts.googleapis.com/css2'], 
     }
     });
