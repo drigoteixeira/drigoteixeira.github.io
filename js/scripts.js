6d56407badea56202fb4f9c3fede107716275829
Rodrigo Teixeira <drigoteixeira@gmail.com>
2020-06-28 12:40:54 -0300

    Update typo and style

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
     
     WebFont.load({
     google: {
-      families: ['Merriweather:400,400i,700,700i,900,900i'],
+      families: ['Source+Serif+Pro:400,400i,700,700i', 'Work+Sans:400'],
       url: ['https://fonts.googleapis.com/css2'], 
     }
     }); 
